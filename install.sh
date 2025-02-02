78a3143f7ef53582313d5d6bdd2438f69b07f7d2
orkkz <affanorakzai@proton.me>
2025-02-02 12:08:05 +0500

    Fixed install.sh

--- install.sh
+++ install.sh
@@ -1,6 +1,8 @@
-sudo su
-apt update
-apt install python3
-apt install openjdk-21-jdk
+sudo apt update
+sudo apt install python3
+sudo apt install openjdk-21-jdk
+curl -fsSL https://deb.nodesource.com/setup_16.x | sudo -E bash -
+sudo apt install -y nodejs
+npm install mineflayer
 pip install discord python-dotenv requests
 python3 app.py
